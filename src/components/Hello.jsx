6fb9b27d463efe354523ba9fde393b655e45f20c
Vruz1103 <vruz2001@gmail.com>
2024-08-28 06:36:17 +0530

    Added work completed till now

---
+++ src/components/Hello.jsx
@@ -0,0 +1,15 @@
+// const name="Rob";
+// function displayMessage(){
+//     return "Wow";
+// }
+function Hello(){
+    // return <h1>Hello from a component{displayMessage()}</h1>
+    // return <h1>Hello from a component</h1> <h1>Wow</h1>;
+    return(
+        <div>
+            <h1>Hello from Vrunda</h1>
+            <h1>Wow</h1>
+        </div>
+    );
+}
+export default Hello;
